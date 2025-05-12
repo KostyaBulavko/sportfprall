f1bc141ac1a5ee35c85ceccc4025ba1f4dec3a17
Kostya Bulavko <58237031+KostyaBulavko@users.noreply.github.com>
2025-05-12 14:36:30 +0300

    Delete version 2.8 directory

--- version 2.8/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Запуск программы Генератор договорів...
-python sport.py
-if %ERRORLEVEL% NEQ 0 (
-    echo Программа завершилась с ошибкой!
-    echo Детали ошибки можно найти в файле error.txt
-    pause
-)
