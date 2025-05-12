8c19442662562a17a274a171b77d152f0a96db87
Kostya Bulavko <58237031+KostyaBulavko@users.noreply.github.com>
2025-05-12 10:39:49 +0300

    Delete 2.7 directory

--- 2.7/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Запуск программы Генератор договорів...
-python fill_contract_gui.py
-if %ERRORLEVEL% NEQ 0 (
-    echo Программа завершилась с ошибкой!
-    echo Детали ошибки можно найти в файле error.txt
-    pause
-)
